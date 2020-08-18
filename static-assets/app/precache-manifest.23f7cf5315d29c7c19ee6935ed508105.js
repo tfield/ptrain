ae43a7d8196a239223362585b41437db0e1cc4a4
admin admin <evaladmin@example.com>
2020-08-18 14:20:10 -0400

    User admin wrote content /static-assets/app/precache-manifest.23f7cf5315d29c7c19ee6935ed508105.js

---
+++ static-assets/app/precache-manifest.23f7cf5315d29c7c19ee6935ed508105.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3f1b9d3fd82870fff3d77066ceab0ba",
+    "url": "/static-assets/app/index.html"
+  },
+  {
+    "revision": "a31a723aeeb1ceaed1c0",
+    "url": "/static-assets/app/static/css/2.96479aa2.chunk.css"
+  },
+  {
+    "revision": "7b4068150983668fddc6",
+    "url": "/static-assets/app/static/css/4.67123b20.chunk.css"
+  },
+  {
+    "revision": "ab549d59ccce0b3f1334",
+    "url": "/static-assets/app/static/css/main.ade22423.chunk.css"
+  },
+  {
+    "revision": "a31a723aeeb1ceaed1c0",
+    "url": "/static-assets/app/static/js/2.d4c8b288.chunk.js"
+  },
+  {
+    "revision": "bb0fd1d56538f577f14e",
+    "url": "/static-assets/app/static/js/3.1e5f042d.chunk.js"
+  },
+  {
+    "revision": "7b4068150983668fddc6",
+    "url": "/static-assets/app/static/js/4.2731a9f2.chunk.js"
+  },
+  {
+    "revision": "ab549d59ccce0b3f1334",
+    "url": "/static-assets/app/static/js/main.3c037eb8.chunk.js"
+  },
+  {
+    "revision": "bf140fa9d54f1eee421f",
+    "url": "/static-assets/app/static/js/runtime~main.f55ec734.js"
+  },
+  {
+    "revision": "cb50982a921f082c28b9b0f8e271fa62",
+    "url": "/static-assets/app/static/media/404.cb50982a.jpg"
+  },
+  {
+    "revision": "8282f1ec8e3598c74fedb340003736f8",
+    "url": "/static-assets/app/static/media/burger.8282f1ec.svg"
+  }
+]);
